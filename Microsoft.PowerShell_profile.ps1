823c8ddb2ef24760895fcca757cd623a729c70fc
lizijing98 <lizijing98@live.com>
2024-05-14 16:31:34 +0800

    feat: 修改omp配置

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,13 @@
 & ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\custom.omp.json" --print) -join "`n"))
 
+Import-Module npm-completion
+Import-Module posh-git
+
+$env:POSH_GIT_ENABLED = $true
+$env:LESSCHARSET='utf-8'
+$GitPromptSettings.EnableStashStatus = $true
+$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [System.Text.Encoding]::GetEncoding(65001);
+
 function runzookeeper {  
   D:\\Program\\kafka_2.13-3.2.1\\bin\\windows\\zookeeper-server-start.bat D:\\Program\\kafka_2.13-3.2.1\\config\\zookeeper.properties
 }
@@ -59,7 +67,7 @@ function getprocess($Port) {
 
 function gitdels($Code) {
   git branch -D @(git branch | select-string "$Code" | Foreach {$_.Line.Trim()})
-  echo 'Remaining branches:'
+  Write-Output 'Remaining branches:'
   git branch
 }
 
@@ -68,15 +76,18 @@ function addWsl2NFW {
   New-NetFirewallRule -DisplayName "WSL" -Direction Inbound  -InterfaceAlias "vEthernet (WSL)"  -Action Allow
 }
 
+function getLastGitLog {
+  $gitlogInfo = git log -1 --pretty=format:"%s"
+  Write-Output $gitlogInfo
+  Set-Clipboard -Value $gitlogInfo
+}
+
 Set-Alias -Name open -Value explorer
 Set-Alias -Name VDI -Value SangforCSClient
 Set-Alias -Name touch -Value New-Item
 Set-Alias -Name dk -Value docker
 # 压缩目录
 Set-Alias -Name zip -Value Compress-Archive
-
-Import-Module npm-completion
-Import-Module posh-git
 
 # PSReadLine Tab 键设置菜单补全
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
